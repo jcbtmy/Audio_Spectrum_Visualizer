c371c3171554c2c471bf541c3bfdcb2dd7b0a8d4
Jacob Toomey <jacobtoomey@pop-os.localdomain>
2020-11-26 15:00:17 -0800

    Adds fullscreen and exit functions

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,50 @@
 
 using namespace std;
 
+struct ScreenDim {
+    int width;
+    int height;
+    int refresh_rate;
+} typedef screenDim;
+
+void get_resolution(int* width, int* height, int* refresh_rate) {
+    const GLFWvidmode * mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+    *width = mode->width;
+    *height = mode->height;
+    *refresh_rate = mode->refreshRate;
+
+}
+
+static void blue_screen(GLFWwindow* window) {
+    glClearColor(0.25, 0.25, 0.25, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+
+}
+
+static void full_screen(GLFWwindow* window, GLFWmonitor* monitor, ScreenDim* dims){
+
+    static bool fullscreen = false;
+
+    glfwPollEvents();
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE)==GLFW_PRESS){
+        glfwSetWindowShouldClose(window, 1);
+        return;
+    }
+    if(glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS){
+        if(!fullscreen) glfwSetWindowMonitor(window, monitor, 0,0, dims->width, dims->height, dims->refresh_rate);
+        if(fullscreen) glfwSetWindowMonitor(window, NULL, 0 ,0 , dims->height, dims->width, dims->refresh_rate);
+        fullscreen = !fullscreen;
+        printf("width: %d || height: %d\n", dims->width ,dims->height);
+    }
+}
+
 
 int main(void){
 
     GLFWwindow* window;
+    GLFWmonitor* monitor;
+    ScreenDim dims;
 
     /* Initialize the library */
     if (!glfwInit())
@@ -22,21 +62,24 @@ int main(void){
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    monitor = glfwGetPrimaryMonitor();
+    get_resolution(&(dims.width), &(dims.height), &(dims.refresh_rate));
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        
+        blue_screen(window);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        full_screen(window, monitor, &dims);
+    
 
         /* Poll for and process events */
         glfwPollEvents();
     }
 
     glfwTerminate();
-    
+
     return 0;
 }
